5f9aaa1545cb1599f28b3b927ffd1d3b6b37599e
abeer-shami <abeer.shami887@gmail.com>
2017-09-13 22:02:37 +0300

    refactoring easy backbone

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -1,17 +1,15 @@
 // Please modify this file!
 
 var Fish = Backbone.Model.extend({
-	Fish.on("onClick",function(fish){
-
-	})
-	Fish.onClick([
-		{name:},])
 
   defaults: {
     name: 'Larry',
     image: 'http://www.google.com',
     description: 'Regular old fish',
     displayInfo: false
+  },
+  toggleDescription:function () {
+    this.set('displayInfo', !this.get('displayInfo'));
+    this.trigger('toggle:description',this)
   }
-
 });
